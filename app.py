c026b538f3bb0bf17504ac9877cd408e5c550cf0
BiggieBu <97216402+BiggieBu@users.noreply.github.com>
2023-04-16 11:59:06 +0000

    Signup and login pages

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,jsonify,request
+from db import login
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
   return render_template('home.html')
+  
+@app.route("/login")
+def login_page():
+  return render_template('login.html')
+  
+@app.route("/login/attempt", methods=["POST"])
+def data_page():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    login(username, password)
+    return jsonify({"username": username, "password": password})
+
+  
+@app.route("/signup")
+def signup():
+  return render_template('signup.html')
+  
+@app.route("/users/apply", methods =['post'])
+def register_user():
+  data = request.form
+  
+  return jsonify(data)
 
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
--- db.py
+++ db.py
@@ -1,9 +1,30 @@
-from sqlalchemy import  create_engine
+from sqlalchemy import  create_engine,text
 
 db_conn_str = "mysql+pymysql://pzzz55wxbxerfsaqc60q:pscale_pw_fMbvXDrFA3mzsPnOwKKRPmrwUVXfpWGnCp22d4cUgRC@aws.connect.psdb.cloud/ridewave?charset=utf8mb4"
 
 engine = create_engine(db_conn_str, connect_args={
   "ssl":{
-    "ca": "/etc/ssl/cert.pem"
+    "ssl_ca": "/etc/ssl/cert.pem"
   }
-})
+},echo=True) 
+def row_to_dict(result_proxy, i):
+    row = result_proxy.fetchone()
+    if row:
+        column_names = result_proxy.keys()
+        values = row
+        diction = dict(zip(column_names, values))
+        return diction
+    else:
+        return {}
+
+def login(name, pword):
+    with engine.connect() as conn:
+        result = conn.execute(text("SELECT username, password FROM Users WHERE username = :val"), {"val": name})
+        d = row_to_dict(result, 0)
+        if not d:
+            print("Username not found")
+        elif d['password'] == pword:
+            print("Welcome")
+        else:
+            print("Wrong password")
+
