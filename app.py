8a43d2d1a8fda1696ef5d41e1a872b561ca6a1d7
BiggieBu <97216402+BiggieBu@users.noreply.github.com>
2023-04-16 21:39:47 +0000

    Added Cluster Creation and Your clusters page

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, flash,session, jsonify
-from db import login
+from db import login,signupinsert,requestinsert,loadclusters,loaddetails,loadpool
 
 app = Flask(__name__)
 app.secret_key = 'my_secret_key'
@@ -13,29 +13,50 @@ def login_page():
   if request.method == 'POST':
     username = request.form['username']
     password = request.form['password']
-    print(username, password)
     result = login(username, password)
     if result == "Welcome":
       session['username'] = username
-      session['password'] = password
-      return redirect('/login/attempt?username={}'.format(username))
+      return redirect('/user/{}'.format(username))
     else:
       flash(result, 'error')
   return render_template('login.html')
 
-@app.route("/login/attempt")
-def data_page():
-  username = session.get('username')
-  password = session.get('password')
-  return render_template('loggedin.html',username = username)
-@app.route("/signup")
+@app.route("/user/<id>")
+def data_page(id):
+  return render_template('loggedin.html',username = id)
+
+@app.route("/signup",methods=['GET','POST'])
 def signup():
+  if request.method=='POST':
+    data=request.form
+    result=signupinsert(data)
+    if result == "Success":
+      return redirect('/login')
+    else:
+      flash(result, 'error')
   return render_template('signup.html')
-
-@app.route("/users/apply", methods=['post'])
-def register_user():
-  data = request.form
-  return jsonify(data)
-
+  
+@app.route("/user/<id>/driver_form",methods=['GET','POST'])
+def driver_form(id):
+  if request.method=='POST':
+    data=request.form
+    result=requestinsert(id,data)
+    if result=="Success":
+      return redirect('/user/{}/clusters'.format(id))
+    else:
+      flash(result,'error')
+  return render_template("driver_form.html",username=id)
+@app.route("/user/<id>/clusters")
+def req(id):
+  clusters=loadclusters(id)
+  return render_template('cluster.html',username=id,clusters=clusters)
+@app.route("/user/<id>/clusters/<cid>")
+def cluster(id,cid):
+  details=loaddetails(cid)
+  passengers=loadpool(cid)
+  return render_template('details.html',username=id,clusters=cid,details=details,passengers=passengers)
 if __name__=="__main__":
   app.run(host='0.0.0.0',debug=True)
+
+
+
--- db.py
+++ db.py
@@ -1,13 +1,14 @@
 from sqlalchemy import  create_engine,text
-
-db_conn_str = "mysql+pymysql://617onh5prl2dg7bx8zmk:pscale_pw_M3XjrIaych0xj9g1Asob8s7LkVXSKqFuYtlET238843@aws.connect.psdb.cloud/ridewave?charset=utf8mb4"
+#Username=ldx9p3zooxyyff0jt88o
+#Password=pscale_pw_5zBWA58QAqQucdH9cBoW5dwKzAiD4ZRyyUu1TKbdPaA
+db_conn_str = "mysql+pymysql://ldx9p3zooxyyff0jt88o:pscale_pw_5zBWA58QAqQucdH9cBoW5dwKzAiD4ZRyyUu1TKbdPaA@aws.connect.psdb.cloud/ridewave?charset=utf8mb4"
 
 engine = create_engine(db_conn_str, connect_args={
   "ssl":{
     "ssl_ca": "/etc/ssl/cert.pem"
   }
 },echo=True) 
-def row_to_dict(result_proxy, i):
+def row_to_dict(result_proxy):
     row = result_proxy.fetchone()
     if row:
         column_names = result_proxy.keys()
@@ -16,17 +17,78 @@ def row_to_dict(result_proxy, i):
         return diction
     else:
         return {}
+def rows_to_list_of_dicts(result_proxy):
+    rows = []
+    for row in result_proxy:
+        column_names = result_proxy.keys()
+        values = row
+        diction = dict(zip(column_names, values))
+        rows.append(diction)
+    return rows
 
 def login(name, pword):
     with engine.connect() as conn:
         result = conn.execute(text("SELECT username, password FROM Users WHERE username = :val"), {"val": name})
-        d = row_to_dict(result, 0)
+        d = row_to_dict(result)
         if not d:
-            return("Username not found")
+          print("no user")
+          return("Username not found")
         elif d['password'] == pword:
-            return("Welcome")
+          print("welcome")
+          return("Welcome")
         else:
-            return("Wrong password")
+          print("no pass")
+          return("Wrong password")
 
-login('Parv','suparv')
 
+
+def signupinsert(data):
+  username = data['uname']
+  firstname = data['fname']
+  lastname = data['lname']
+  email = data['mail']
+  phoneNo = data['phone']
+  password = data['pass']
+  try:
+        with engine.connect() as conn:
+           conn.execute(
+               text("insert into Users(`username`, `userFirstName`, `userLastName`, `EmailID`, `PhoneNo.`, `password`)values(:z,:x,:y,:a,:b,:c)"),
+              {"x": firstname, "y": lastname, "z": username, "a": email, "b": phoneNo, "c": password}
+           )
+        return "Success"
+  except Exception as e:
+        return str(e)
+def requestinsert(id,data):
+  carname= data['carname']
+  carno= data['carno']
+  cost=data['cost']
+  initx=data['initx']
+  inity=data['inity']
+  finalx=data['finalx']
+  finaly=data['finaly']
+  leavedate=data['leavedate']
+  leavetime=data['leavetime']
+  seats=data['seats']
+  try:
+        with engine.connect() as conn:
+           conn.execute(
+               text("insert into Cluster(`adminusername`,`carname`, `carNo`, `cost`, `initiallocationx`, `initiallocationy`, `finallocationx`,`finallocationy`,`leavingtime`,`leavedate`,`seats`)values(:v0,:v1,:v2,:v3,:v4,:v5,:v6,:v7,:v8,:v9,:v10)"),
+              {"v0":id,"v1": carname, "v2": carno, "v3": cost, "v4": initx, "v5": inity, "v6": finalx,"v7":finaly,"v9":leavedate,"v8":leavetime,"v10":seats}
+           )
+        return "Success"
+  except Exception as e:
+        return str(e)
+def loadclusters(id):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM Cluster where adminusername=:val"), {"val": id})
+    return rows_to_list_of_dicts(result)
+
+def loaddetails(cid):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM Cluster where clusterid=:val"), {"val": cid})
+    return row_to_dict(result)
+
+def loadpool(cid):
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM Users where clusterid=:val"), {"val": cid})
+    return rows_to_list_of_dicts(result)
